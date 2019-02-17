b762c63b3f832205b70cc3e690507a6614ed6e73
Cody Chinn <cchinn@challenge-mfg.com>
2019-02-17 15:12:59 -0800

    Setting gravity to top on toast messages for challenge one

--- app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -2,6 +2,7 @@ package android.bignerdranch.com;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,9 +21,18 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+
+                // Creating the correct toast Toast object
+                Toast correctToast = new Toast(QuizActivity.this);
+
+                //setting message for correct toast
+                correctToast.makeText(QuizActivity.this,
                         R.string.correct_toast,
                         Toast.LENGTH_SHORT).show();
+
+                // Setting the Gravity to Top for Challenge One
+                correctToast.setGravity(Gravity.TOP, 0, 0);
+
             }
         });
 
@@ -30,9 +40,17 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+
+                // Creating the incorrect toast Toast object
+                Toast incorrectToast = new Toast(QuizActivity.this);
+
+                // Setting the message for incorrect toast
+                incorrectToast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
                         Toast.LENGTH_SHORT).show();
+
+                // Setting the Gravity to Top for Challenge One
+                incorrectToast.setGravity(Gravity.TOP, 0, 0);
             }
         });
 
