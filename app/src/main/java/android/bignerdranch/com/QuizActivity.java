4dbec358ed4154774c62b84ae640225718a2894f
Cody Chinn <cdchinn14@gmail.com>
2019-02-18 21:35:34 -0500

    Adding Log tags to experiment with activity lifecycle

--- app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -2,6 +2,7 @@ package android.bignerdranch.com;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -91,7 +93,35 @@ public class QuizActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
